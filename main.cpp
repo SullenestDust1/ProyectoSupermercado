96b857860c7a79bbfde801173e770c3093c5f2cf
juan <juanl1996@hotmail.com>
2018-03-09 22:42:48 -0400

    main con errores

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Librerias/Lista/Lista.h"
+/*#include "Librerias/Lista/Lista.h"
 #include "Librerias/Cola/Cola.h"
 #include "Librerias/Pila/Pila.h"
 int main() {
@@ -8,4 +8,41 @@ int main() {
     Pila<int> pila;
     return 0;
 }
-//
+//*/
+
+
+#include "Controlador/Controlador.h"
+#include "Librerias/Genericas/VGeneral.h"
+
+int main()
+{
+    int opc;
+    VGeneral  vg;
+    Controlador c;
+    do
+    {
+        vg.Limpiar();
+        vg.ImprimirLineasBlanco(2);
+        vg.Limpiar();
+        vg.ImprimirEncabezado("\n      M E N U  O P C I O N E S\n","      =======  ===============");
+        vg.ImprimirMensaje("   1. CARGAR DATOS PELICULAS\n");
+        vg.ImprimirMensaje("   2. CARGARr LAS SALAS\n");
+        vg.ImprimirMensaje("   3. PROCESAR ENTRADAS\n");
+        vg.ImprimirMensaje("   4. REPORTE CINEX\n");
+        vg.ImprimirMensaje("   5. FINALIZAR\n\n");
+        opc = vg.LeerValidarNro("   SELECCIONE SU OPCION : ",1,5);
+        switch (opc)
+        {
+            case 1: //c.CargarPeliculas();
+                break;
+            case 2: //c.CargarSalas();
+                break;
+            case 3: //c.ProcesarEntradas();
+                break;
+            case 4: //c.ReporteCinex();
+                break;
+        }
+    }
+    while(opc != 5);
+    return 0;
+}
