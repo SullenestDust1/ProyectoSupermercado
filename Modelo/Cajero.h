19fc2b7e2b31f92b16ad1198806752919bfc3a84
Marco Saenz <marco.saenz262@gmail.com>
2018-03-09 20:59:09 -0400

    Merge branch 'master' of /home/marco/CLionProjects/NoNumerica1/ProyectoSupermercado with conflicts.

---
+++ Modelo/Cajero.h
@@ -0,0 +1,14 @@
+//
+// Created by marco on 3/9/18.
+//
+
+#ifndef PROYECTOSUPERMERCADO_CAJERO_H
+#define PROYECTOSUPERMERCADO_CAJERO_H
+
+
+class Cajero {
+
+};
+
+
+#endif //PROYECTOSUPERMERCADO_CAJERO_H
