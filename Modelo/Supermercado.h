01e3b5bee7d856b2fcefbf1a8980a5ad7797f075
Marco Saenz <marco.saenz262@gmail.com>
2018-03-09 20:36:04 -0400

    Actualizacion Clase SuperMercado

--- Modelo/Supermercado.h
+++ Modelo/Supermercado.h
@@ -5,8 +5,10 @@
 #ifndef PROYECTOSUPERMERCADO_SUPERMERCADO_H
 #define PROYECTOSUPERMERCADO_SUPERMERCADO_H
 
-
+#include "../Librerias/Lista/Lista.h"
+#include "CajaRegistradora.h"
 class Supermercado {
+    Lista<CajaRegistradora> ListaCajas;
 
 };
 
