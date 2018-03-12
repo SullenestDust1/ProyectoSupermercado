7abbc65a8cd46025ab58ce2c869b2af424f264c4
Marco Saenz <marco.saenz262@gmail.com>
2018-03-12 01:28:04 -0400

    Actualizacion Cajero

--- Modelo/MCajero.h
+++ Modelo/MCajero.h
@@ -1,15 +1,18 @@
 //
 // Created by marco on 3/9/18.
 //
+#ifndef PROYECTOSUPERMERCADO_MCAJERO_H
+#define PROYECTOSUPERMERCADO_MCAJERO_H
+
 #include "../Librerias/Genericas/MArticulo.h"
 #include "../Librerias/Genericas/MHumano.h"
 
-#ifndef PROYECTOSUPERMERCADO_MCAJERO_H
-#define PROYECTOSUPERMERCADO_MCAJERO_H
 class MCajero: public MHumano{
-public:
-
 private:
+    string codigo;
+public:
+    const string &getCodigo() const;
+    void setCodigo(const string &codigo);
 
 };
 
